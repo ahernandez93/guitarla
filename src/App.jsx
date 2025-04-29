46bd08bed5f01986b2d9a12871cc34420fe122b4
Allan <allan_hc@outlook.com>
2025-04-29 17:53:47 -0600

    Hasta leccion 72

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,38 @@ import { db } from './data/db'
 function App() {
 
   const [data, setData] = useState(db)
+  const [cart, setCart] = useState([])
+
+  function addToCart(item) {
+    const itemExists = cart.findIndex(guitar => guitar.id === item.id)
+    if(itemExists >= 0){
+      const updatedCart = [...cart]
+      updatedCart[itemExists].quantity++
+      setCart(updatedCart)
+    }else{
+      item.quantity = 1
+      setCart([...cart, item])
+    }
+  }
 
   return (
     <>
       <Header></Header>
       <main className="container-xl mt-5">
         <h2 className="text-center">Nuestra Colección</h2>
-
         <div className="row mt-5">
-          {data.map(() => (
-            <Guitar></Guitar>
+          {data.map((guitar) => (
+            <Guitar
+              key={guitar.id}
+              guitar={guitar}
+              setCart={setCart}
+              addToCart={addToCart}
+            >
+            </Guitar>
           ))}
 
         </div>
       </main>
-
-
       <footer className="bg-dark mt-5 py-5">
         <div className="container-xl">
           <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
