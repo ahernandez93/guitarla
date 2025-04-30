d19b2223af5734b5030b879ce98a49beac5b6ec3
Allan <allan_hc@outlook.com>
2025-04-30 16:28:44 -0600

    Hasta leccion 81

--- src/App.jsx
+++ src/App.jsx
@@ -8,21 +8,63 @@ function App() {
   const [data, setData] = useState(db)
   const [cart, setCart] = useState([])
 
+  const MAX_ITEMS = 5
+  const MIN_ITEMS = 1
+
   function addToCart(item) {
     const itemExists = cart.findIndex(guitar => guitar.id === item.id)
-    if(itemExists >= 0){
+    if (itemExists >= 0) {
+      if (cart[itemExists].quantity >= MAX_ITEMS) {
+        alert('No puedes agregar más de 5 unidades')
+        return
+      }
       const updatedCart = [...cart]
       updatedCart[itemExists].quantity++
       setCart(updatedCart)
-    }else{
+    } else {
       item.quantity = 1
       setCart([...cart, item])
     }
   }
 
+  function removeFromCart(id) {
+    setCart(prevCart => prevCart.filter(guitar => guitar.id !== id))
+  }
+
+  function increaseQuantity(id) {
+    const updatedCart = cart.map(item => {
+      if (item.id === id && item.quantity < MAX_ITEMS) {
+        return { ...item, quantity: item.quantity + 1 }
+      }
+      return item
+    })
+    setCart(updatedCart)
+  }
+
+  function decreaseQuantity(id) {
+    const updatedCart = cart.map(item => {
+      if (item.id === id && item.quantity > MIN_ITEMS) {
+        return { ...item, quantity: item.quantity - 1 }
+      }
+      return item
+    })
+    setCart(updatedCart)
+  }
+
+  function clearCart() {
+    setCart([])
+  }
+
   return (
     <>
-      <Header></Header>
+      <Header
+        cart={cart}
+        removeFromCart={removeFromCart}
+        increaseQuantity={increaseQuantity}
+        decreaseQuantity={decreaseQuantity}
+        clearCart={clearCart}
+      ></Header>
+
       <main className="container-xl mt-5">
         <h2 className="text-center">Nuestra Colección</h2>
         <div className="row mt-5">
@@ -32,8 +74,7 @@ function App() {
               guitar={guitar}
               setCart={setCart}
               addToCart={addToCart}
-            >
-            </Guitar>
+            ></Guitar>
           ))}
 
         </div>
